77be2bb80e858fc372e2277cbaed7c97213c02a9
Kurtis Nusbaum <kurtis@kurtiss-mbp.lan>
2017-09-10 17:08:09 -0700

    add more to setup.sh

--- setup.sh
+++ setup.sh
@@ -1,12 +1,45 @@
-curdir=$(pwd)
-git_conf_path="$curdir/git_config_common"
+#!/bin/bash
+ensurebrew () {
+ if type brew 2>/dev/null
+ then
+   echo "Brew detected. No need to install"
+ else
+   echo "Brew not detected. Installing..."
+   /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+ fi 
+}
+
+
+macinstall() {
+   ensurebrew
+   echo "Installing reattach-user-namespace"
+   brew install reattach-to-user-namespace
+   echo "Installing nvim"
+   brew install nvim
+}
+
+
+if [ "$(uname)" = "Darwin" ]
+then
+  echo "Darwin detected. Installing necessary software"
+  macinstall
+fi
+
+FILES_DIR=$(dirname "${BASH_SOURCE[0]}")
+
+echo "Including common git config"
+git_conf_path="$FILES_DIR/git_config_common"
 git config --global include.path "$git_conf_path"
 
-echo "source $curdir/bash_common" >> $HOME/.bash_profile
+echo "Adding common bash config"
+echo "source $FILES_DIR/bash_common" >> $HOME/.bash_profile
 
+echo "Linking tmux config"
+ln -s $FILES_DIR/tmux.conf $HOME/.tmux.conf
+
+echo "Linking nvim config"
 mkdir -p $HOME/.config/nvim
-ln -s vim_conf $HOME/.config/nvim/init.vim
+ln -s $FILES_DIR/vim_conf $HOME/.config/nvim/init.vim
 
-ln -s tmux.conf $HOME/.tmux.conf
-
+echo "Installing Plug for vim"
 curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs  https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
