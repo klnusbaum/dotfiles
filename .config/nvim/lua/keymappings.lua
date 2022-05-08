8d44a0133f8f86a76ff6bd8c64d2d4f40bd4e623
Kurtis Nusbaum <kcommiter@gmail.com>
2022-05-07 20:27:45 -0700

    simplify keymappings

--- .config/nvim/lua/keymappings.lua
+++ .config/nvim/lua/keymappings.lua
@@ -1,14 +1,11 @@
 local M = {}
 
-local ext_opts = require("options").ext_opts
-
 function M.tkmap(shortcut, action)
     vim.keymap.set('t', shortcut, action)
 end
 
-function M.nkmap(shortcut, action, opts)
-    local all_opts = ext_opts({ noremap = true }, opts)
-    vim.keymap.set('n', '<leader>' .. shortcut, action, opts)
+function M.nkmap(shortcut, action)
+    vim.keymap.set('n', '<leader>' .. shortcut, action)
 end
 
 return M
