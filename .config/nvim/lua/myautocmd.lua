5ba30718fabd5f768915143ada93e94eac17148b
Kurtis Nusbaum <kcommiter@gmail.com>
2022-05-13 21:44:58 -0700

    make create_personal_group a little nicer

--- .config/nvim/lua/myautocmd.lua
+++ .config/nvim/lua/myautocmd.lua
@@ -1,16 +1,18 @@
 local M = {}
 
+local GROUP_NAME = 'Personal'
 local ext_opts = require('options').ext_opts
 
 function M.create_personal_group()
-  local personal_group_id = vim.api.nvim_create_augroup('Personal', { clear = true })
-  local personal_group = {
+  local personal_group_id = vim.api.nvim_create_augroup(GROUP_NAME, { clear = true })
+  return {
     new_autocmd = function(events, opts)
       all_opts = ext_opts({ group = personal_group_id }, opts)
       vim.api.nvim_create_autocmd(events, all_opts)
-    end
+    end,
+    name = GROUP_NAME,
+    id = personal_group_id,
   }
-  return personal_group
 end
 
 return M
