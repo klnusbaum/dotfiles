15024071372836fc73baeb60dbd877194a644460
Kurtis Nusbaum <kcommiter@gmail.com>
2022-05-14 14:34:42 -0700

    fix variable to be local

--- .config/nvim/lua/myautocmd.lua
+++ .config/nvim/lua/myautocmd.lua
@@ -7,7 +7,7 @@ function M.create_personal_group()
   local personal_group_id = vim.api.nvim_create_augroup(GROUP_NAME, { clear = true })
   return {
     new_autocmd = function(events, opts)
-      all_opts = ext_opts({ group = personal_group_id }, opts)
+      local all_opts = ext_opts({ group = personal_group_id }, opts)
       vim.api.nvim_create_autocmd(events, all_opts)
     end,
     name = GROUP_NAME,
