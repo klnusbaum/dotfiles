e268ffbf02dfc2f0de5904a8b7b10ad17baf8a9d
Kurtis Nusbaum <kcommiter@gmail.com>
2022-06-06 09:54:40 -0700

    fix bug with delete in Augroup

--- .config/nvim/lua/kautocmd.lua
+++ .config/nvim/lua/kautocmd.lua
@@ -18,7 +18,7 @@ function Augroup:add_cmd(events, opts)
 end
 
 function Augroup:delete()
-    vim.api.delete_augroup_by_id(self.id)
+    vim.api.nvim_del_augroup_by_id(self.id)
 end
 
 M.Augroup = Augroup
