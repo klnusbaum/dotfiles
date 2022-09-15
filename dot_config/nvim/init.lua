2fce1f3a07e4ed1a2ab51593ea5e68736eb783e2
Kurtis Nusbaum <kcommiter@gmail.com>
2022-09-15 14:06:24 -0700

    forgot to remove pluging stuff from init.lua

--- dot_config/nvim/init.lua
+++ dot_config/nvim/init.lua
@@ -1,6 +1,9 @@
 -- Switch to utilizing special nvim path if it exists
 vim.env.PATH = vim.env.NVIM_PATH or vim.env.PATH
 
+-- load plugins
+require("plugins")
+
 -- helper functions
 local fn = vim.fn
 local kt_map = require("keymappings").kt_map
@@ -14,39 +17,6 @@ local create_diff = require("phab").create_diff
 
 local personal_group = Augroup:new("personal")
 
--- load plugins
-local install_path = fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
-if fn.empty(fn.glob(install_path)) > 0 then
-  packer_bootstrap = fn.system({'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path})
-end
-
-require('packer').startup(function(use)
-  use 'wbthomason/packer.nvim'
-  use 'ojroques/vim-oscyank'
-  use 'neovim/nvim-lspconfig'
-  use 'tpope/vim-commentary'
-  use 'tpope/vim-fugitive'
-  use 'tpope/vim-surround'
-  use 'vim-airline/vim-airline'
-  use 'vim-airline/vim-airline-themes'
-  use 'nvim-lua/plenary.nvim'
-  use 'rafcamlet/nvim-luapad'
-  use 'L3MON4D3/LuaSnip'
-  use 'saadparwaiz1/cmp_luasnip'
-  use 'hrsh7th/nvim-cmp'
-  use 'hrsh7th/cmp-nvim-lsp'
-  use 'hrsh7th/cmp-buffer'
-  use {
-    'nvim-telescope/telescope.nvim',
-    requires = { {'nvim-lua/plenary.nvim'} }
-  }
-
-  -- Automatically set up your configuration after cloning packer.nvim
-  -- Put this at the end after all plugins
-  if packer_bootstrap then
-    require('packer').sync()
-  end
-end)
 
 -- Misc options settings
 vim.opt.nu=true
