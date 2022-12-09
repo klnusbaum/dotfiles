54a8ff30499cb544b869b491c577cade775d3139
Kurtis Nusbaum <kcommiter@gmail.com>
2022-12-09 23:12:11 +0000

    force term gui colors no matter what in nvim

--- dot_config/nvim/init.lua
+++ dot_config/nvim/init.lua
@@ -33,6 +33,7 @@ vim.opt.tabstop=4
 vim.opt.shiftwidth=4
 vim.opt.expandtab=true
 vim.opt.smartindent=true
+vim.opt.termguicolors=true
 vim.opt.clipboard="unnamedplus"
 vim.opt.splitbelow=true
 vim.opt.splitright=true
