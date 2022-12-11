a59c962979be2bdd92b3a1422d42668d55a80705
Kurtis Nusbaum <kcommiter@gmail.com>
2022-12-10 20:45:35 -0800

    disable third party check for lua lsp since it's not applicable to neovim

--- dot_config/nvim/init.lua
+++ dot_config/nvim/init.lua
@@ -184,6 +184,7 @@ local neovim_lua_lsp_settings = {
     workspace = {
       -- Make the server aware of Neovim runtime files
       library = vim.api.nvim_get_runtime_file("", true),
+      checkThirdParty = false, -- turn off to avoid third part check prompt
     },
     -- Do not send telemetry data containing a randomized but unique identifier
     telemetry = {
