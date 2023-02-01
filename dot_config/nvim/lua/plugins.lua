8eb576720ddb62ed1c5c87de4fe9365b1c137178
Kurtis Nusbaum <kcommiter@gmail.com>
2023-02-01 21:27:56 +0000

    add more completion plugins

--- dot_config/nvim/lua/plugins.lua
+++ dot_config/nvim/lua/plugins.lua
@@ -22,8 +22,10 @@ return require('packer').startup(function(use)
   use 'L3MON4D3/LuaSnip'
   use 'saadparwaiz1/cmp_luasnip'
   use 'hrsh7th/nvim-cmp'
-  use 'hrsh7th/cmp-nvim-lsp'
   use 'hrsh7th/cmp-buffer'
+  use 'hrsh7th/cmp-path'
+  use 'hrsh7th/cmp-nvim-lsp'
+  use 'hrsh7th/cmp-nvim-lsp-signature-help'
   use 'folke/tokyonight.nvim'
   use 'Rigellute/shades-of-purple.vim'
   use { "catppuccin/nvim", as = "catppuccin" }
