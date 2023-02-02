34402334c60b3810ae49e0538aca8c3437fca3ba
Kurtis Nusbaum <kcommiter@gmail.com>
2023-02-02 00:24:11 +0000

    organize dependencies for plugins

--- dot_config/nvim/lua/plugins.lua
+++ dot_config/nvim/lua/plugins.lua
@@ -12,29 +12,37 @@ end
 vim.opt.rtp:prepend(lazypath)
 
 require('lazy').setup({
-  -- nvim-cmp
-  'L3MON4D3/LuaSnip',
-  'saadparwaiz1/cmp_luasnip',
-  'hrsh7th/nvim-cmp',
-  'hrsh7th/cmp-buffer',
-  'hrsh7th/cmp-path',
-  'hrsh7th/cmp-nvim-lsp',
-  'hrsh7th/cmp-nvim-lsp-signature-help',
+    -- nvim-cmp
+    {
+        'hrsh7th/nvim-cmp',
+        dependencies = {
+            'hrsh7th/cmp-buffer',
+            'hrsh7th/cmp-path',
+            'hrsh7th/cmp-nvim-lsp',
+            'hrsh7th/cmp-nvim-lsp-signature-help',
+            'L3MON4D3/LuaSnip',
+            'saadparwaiz1/cmp_luasnip',
+        },
+    },
 
-  -- telescope
-  'nvim-telescope/telescope.nvim',
-  'nvim-lua/plenary.nvim',
+    -- telescope
+    {
+        'nvim-telescope/telescope.nvim',
+        dependencies = {'nvim-lua/plenary.nvim'},
+    },
 
-  -- lualine
-  'nvim-lualine/lualine.nvim',
-  'nvim-tree/nvim-web-devicons',
+    -- lualine
+    {
+        'nvim-lualine/lualine.nvim',
+        dependencies = {'nvim-tree/nvim-web-devicons'},
+    },
 
-  -- others
-  'ojroques/vim-oscyank',
-  'neovim/nvim-lspconfig',
-  'tpope/vim-commentary',
-  'tpope/vim-fugitive',
-  'tpope/vim-surround',
-  'folke/tokyonight.nvim',
-  'Rigellute/shades-of-purple.vim',
+    -- others
+    'ojroques/vim-oscyank',
+    'neovim/nvim-lspconfig',
+    'tpope/vim-commentary',
+    'tpope/vim-fugitive',
+    'tpope/vim-surround',
+    'folke/tokyonight.nvim',
+    'Rigellute/shades-of-purple.vim',
 })
