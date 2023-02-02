64afb828d1d0c3ababdf58c8b00f04df3991d1c3
Kurtis Nusbaum <kcommiter@gmail.com>
2023-02-02 00:03:53 +0000

    switch from packer to lazy

--- dot_config/nvim/lua/plugins.lua
+++ dot_config/nvim/lua/plugins.lua
@@ -1,47 +1,40 @@
-local ensure_packer = function()
-  local fn = vim.fn
-  local install_path = fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
-  if fn.empty(fn.glob(install_path)) > 0 then
-    fn.system({'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path})
-    vim.cmd [[packadd packer.nvim]]
-    return true
-  end
-  return false
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+	vim.fn.system({
+		"git",
+		"clone",
+		"--filter=blob:none",
+		"https://github.com/folke/lazy.nvim.git",
+		"--branch=stable", -- latest stable release
+		lazypath,
+	})
 end
+vim.opt.rtp:prepend(lazypath)
 
-local packer_bootstrap = ensure_packer()
+require('lazy').setup({
+  -- nvim-cmp
+  'L3MON4D3/LuaSnip',
+  'saadparwaiz1/cmp_luasnip',
+  'hrsh7th/nvim-cmp',
+  'hrsh7th/cmp-buffer',
+  'hrsh7th/cmp-path',
+  'hrsh7th/cmp-nvim-lsp',
+  'hrsh7th/cmp-nvim-lsp-signature-help',
 
-return require('packer').startup(function(use)
-  use 'wbthomason/packer.nvim'
-  use 'ojroques/vim-oscyank'
-  use 'neovim/nvim-lspconfig'
-  use 'tpope/vim-commentary'
-  use 'tpope/vim-fugitive'
-  use 'tpope/vim-surround'
-  use 'rafcamlet/nvim-luapad'
-  use 'L3MON4D3/LuaSnip'
-  use 'saadparwaiz1/cmp_luasnip'
-  use 'hrsh7th/nvim-cmp'
-  use 'hrsh7th/cmp-buffer'
-  use 'hrsh7th/cmp-path'
-  use 'hrsh7th/cmp-nvim-lsp'
-  use 'hrsh7th/cmp-nvim-lsp-signature-help'
-  use 'folke/tokyonight.nvim'
-  use 'Rigellute/shades-of-purple.vim'
-  use { "catppuccin/nvim", as = "catppuccin" }
-  use {
-    'nvim-telescope/telescope.nvim',
-    requires = { {'nvim-lua/plenary.nvim'} }
-  }
-  use {
-    'nvim-lualine/lualine.nvim',
-    requires = { 'nvim-tree/nvim-web-devicons'}
-  }
+  -- telescope
+  'nvim-telescope/telescope.nvim',
+  'nvim-lua/plenary.nvim',
 
+  -- lualine
+  'nvim-lualine/lualine.nvim',
+  'nvim-tree/nvim-web-devicons',
 
-  -- Automatically set up your configuration after cloning packer.nvim
-  -- Put this at the end after all plugins
-  if packer_bootstrap then
-    require('packer').sync()
-  end
-end)
+  -- others
+  'ojroques/vim-oscyank',
+  'neovim/nvim-lspconfig',
+  'tpope/vim-commentary',
+  'tpope/vim-fugitive',
+  'tpope/vim-surround',
+  'folke/tokyonight.nvim',
+  'Rigellute/shades-of-purple.vim',
+})
