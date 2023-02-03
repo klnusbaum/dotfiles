4217bb437e2f910c18a9890e4b92007223e0ff18
Kurtis Nusbaum <kcommiter@gmail.com>
2023-02-03 17:45:22 +0000

    reorganize plugins

---
+++ dot_config/nvim/lua/plugins/cmp.lua
@@ -0,0 +1,12 @@
+return {
+    'hrsh7th/nvim-cmp',
+    dependencies = {
+        'hrsh7th/cmp-buffer',
+        'hrsh7th/cmp-path',
+        'hrsh7th/cmp-nvim-lsp',
+        'hrsh7th/cmp-nvim-lsp-signature-help',
+        'L3MON4D3/LuaSnip',
+        'saadparwaiz1/cmp_luasnip',
+        'hrsh7th/cmp-emoji',
+    },
+}
