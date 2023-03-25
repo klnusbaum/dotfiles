288e7678aa9e2f1324d2b78af60a93dc96da82f9
Kurtis Nusbaum <kcommiter@gmail.com>
2023-03-25 17:28:11 +0000

    add ability to oscyank a line

--- dot_config/nvim/lua/plugins/osc52.lua
+++ dot_config/nvim/lua/plugins/osc52.lua
@@ -7,6 +7,7 @@ return {
 
         osc52.setup(opts)
         kn_l_map('y', osc52.copy_operator, {expr = true})
+        kn_l_map('yy', '<leader>y_', {remap = true})
         kv_l_map('y', osc52.copy_visual)
     end
 }
