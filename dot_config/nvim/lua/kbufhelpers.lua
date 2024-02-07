c128bcf7d9fd24489c3e8d26ffe6ae7d4054ba5a
Kurtis Nusbaum <kcommiter@gmail.com>
2024-02-07 23:12:45 +0000

    refactor kbufhelpers

--- dot_config/nvim/lua/kbufhelpers.lua
+++ dot_config/nvim/lua/kbufhelpers.lua
@@ -1,10 +1,9 @@
 local M = {}
 
--- TODO should just use nvim_get_current_buffer instead
-local CURRENT_BUFFER = 0
 local ALL_LINES = -1
 local get_lines = vim.api.nvim_buf_get_lines
 local set_lines = vim.api.nvim_buf_set_lines
+local get_current_buf = vim.api.nvim_get_current_buf
 
 local function split_to_table(contents)
   local result = {};
@@ -15,13 +14,13 @@ local function split_to_table(contents)
 end
 
 function M.current_buf_contents()
-  local lines = get_lines(CURRENT_BUFFER, 0, ALL_LINES, false)
+  local lines = get_lines(get_current_buf(), 0, ALL_LINES, false)
   return table.concat(lines, "\n")
 end
 
 function M.set_current_buf_contents(contents)
   local lines = split_to_table(contents)
-  set_lines(CURRENT_BUFFER, 0, ALL_LINES, false, lines)
+  set_lines(get_current_buf(), 0, ALL_LINES, false, lines)
 end
 
 return M
