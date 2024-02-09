68385048802dba2d48deb42f8125a145238db1f2
Kurtis Nusbaum <kcommiter@gmail.com>
2024-02-09 06:19:40 +0000

    add mini sessions

---
+++ dot_config/nvim/lua/plugins/mini.lua
@@ -0,0 +1,23 @@
+local kn_l_map = require("keymappings").kn_l_map
+
+return {
+    'echasnovski/mini.nvim',
+    version = false,
+    config = function()
+        local sessions = require("mini.sessions")
+        sessions.setup()
+        kn_l_map('Sc', function()
+            vim.ui.input({
+                prompt = 'Create new session named: ',
+                default = vim.fs.basename(vim.fn.getcwd()),
+            }, function(session_name)
+                if session_name ~= nil then
+                    sessions.write(session_name, {})
+                end
+            end)
+        end, { desc = 'Create new session' })
+        kn_l_map('Sr', function() sessions.select('read', {}) end, { desc = 'Read session' })
+        kn_l_map('Sw', function() sessions.select('write', {}) end, { desc = 'Write session' })
+        kn_l_map('Sd', function() sessions.select('delete', {}) end, { desc = 'Delete session' })
+    end
+}
--- dot_config/nvim/lua/plugins/telescope.lua
+++ dot_config/nvim/lua/plugins/telescope.lua
@@ -1,4 +1,19 @@
 return {
     'nvim-telescope/telescope.nvim',
-    dependencies = {'nvim-lua/plenary.nvim'},
+    dependencies = {
+        'nvim-lua/plenary.nvim',
+        'nvim-telescope/telescope-ui-select.nvim',
+    },
+    config = function()
+        local telescope = require('telescope')
+        local themes = require('telescope.themes')
+        telescope.setup({
+            extensions = {
+                ["ui-select"] = {
+                    themes.get_dropdown(),
+                },
+            },
+        })
+        telescope.load_extension('ui-select')
+    end
 }
