662e9c0eba45e3236b6c58ddf4f310d4cd34d32a
Kurtis Nusbaum <kcommiter@gmail.com>
2024-02-11 13:32:24 -0800

    remove custom keymapping code

--- dot_config/nvim/lua/keymappings.lua
+++
@@ -1,7 +0,0 @@
-local M = {}
-
-function M.kv_l_map(shortcut, action, opts)
-    vim.keymap.set('v', '<leader>' .. shortcut, action, opts)
-end
-
-return M
--- dot_config/nvim/lua/plugins/osc52.lua
+++ dot_config/nvim/lua/plugins/osc52.lua
@@ -2,11 +2,10 @@ return {
     'ojroques/nvim-osc52',
     config = function(_, opts)
         local osc52 = require 'osc52'
-        local kv_l_map = require("keymappings").kv_l_map
 
         osc52.setup(opts)
         vim.keymap.set('n', '<leader>y', osc52.copy_operator, { expr = true })
         vim.keymap.set('n', '<leader>yy', '<leader>y_', { remap = true })
-        kv_l_map('y', osc52.copy_visual)
+        vim.keymap.set('v', '<leader>y', osc52.copy_visual)
     end
 }
