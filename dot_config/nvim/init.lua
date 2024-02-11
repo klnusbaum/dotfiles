347f9e085de93828bd533550fc489abcfaccda89
Kurtis Nusbaum <kcommiter@gmail.com>
2024-02-10 22:15:10 -0800

    move cmp config to plugin file

--- dot_config/nvim/init.lua
+++ dot_config/nvim/init.lua
@@ -88,36 +88,6 @@ kn_l_map("ns", function()
     vim.opt.spell = false
 end)
 
--- nvim-cmp
-local cmp = require('cmp')
-cmp.setup({
-    snippet = {
-        -- REQUIRED - you must specify a snippet engine
-        expand = function(args)
-            require('luasnip').lsp_expand(args.body) -- For `luasnip` users.
-        end,
-    },
-    mapping = cmp.mapping.preset.insert({
-        ['<C-b>'] = cmp.mapping.scroll_docs(-4),
-        ['<C-f>'] = cmp.mapping.scroll_docs(4),
-        ['<C-Space>'] = cmp.mapping.complete(),
-        ['<C-e>'] = cmp.mapping.abort(),
-        ['<CR>'] = cmp.mapping.confirm({ select = false }),
-    }),
-    sources = cmp.config.sources({
-        { name = 'nvim_lsp' },
-        { name = 'nvim_lsp_signature_help' },
-        { name = 'buffer' },
-        { name = 'emoji' },
-        { name = 'path' },
-        { name = 'luasnip' },
-    }),
-    window = {
-        completion = cmp.config.window.bordered(),
-        documentation = cmp.config.window.bordered(),
-    }
-})
-
 -- lsp settings
 personal_group:add_cmd("BufWritePre", {
     pattern = { '*.bzl', '*.bazel', '*.star', '*.rs', '*.go', '*.lua', '*.js', '*.py' },
--- dot_config/nvim/lua/plugins/cmp.lua
+++ dot_config/nvim/lua/plugins/cmp.lua
@@ -9,4 +9,33 @@ return {
         'saadparwaiz1/cmp_luasnip',
         'hrsh7th/cmp-emoji',
     },
+    config = function()
+        local cmp = require('cmp')
+        cmp.setup({
+            snippet = {
+                expand = function(args)
+                    require('luasnip').lsp_expand(args.body)
+                end,
+            },
+            mapping = cmp.mapping.preset.insert({
+                ['<C-b>'] = cmp.mapping.scroll_docs(-4),
+                ['<C-f>'] = cmp.mapping.scroll_docs(4),
+                ['<C-Space>'] = cmp.mapping.complete(),
+                ['<C-e>'] = cmp.mapping.abort(),
+                ['<CR>'] = cmp.mapping.confirm({ select = false }),
+            }),
+            sources = cmp.config.sources({
+                { name = 'nvim_lsp' },
+                { name = 'nvim_lsp_signature_help' },
+                { name = 'buffer' },
+                { name = 'emoji' },
+                { name = 'path' },
+                { name = 'luasnip' },
+            }),
+            window = {
+                completion = cmp.config.window.bordered(),
+                documentation = cmp.config.window.bordered(),
+            }
+        })
+    end,
 }
