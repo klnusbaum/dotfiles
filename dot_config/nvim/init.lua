67326e27d14faa581035e83b42fdca8b807729af
Kurtis Nusbaum <kcommiter@gmail.com>
2024-02-11 13:41:40 -0800

    add fugitive keymaps to plugin config

--- dot_config/nvim/init.lua
+++ dot_config/nvim/init.lua
@@ -148,26 +148,6 @@ lspconfig["lua_ls"].setup({
     },
 })
 
-
--- Fugitive (git) customizations
-vim.keymap.set('n', '<leader>ggu', function()
-    vim.cmd("Git add --update")
-    vim.notify('git added all updated files')
-end)
-vim.keymap.set('n', '<leader>gga', function() vim.cmd("Git commit --amend --no-edit") end)
-vim.keymap.set('n', '<leader>ggc', function()
-    vim.ui.input({ prompt = 'Commit message: ' }, function(input)
-        if input then
-            vim.cmd('Git commit -m "' .. input .. '"')
-        else
-            vim.notify("Commit aborted")
-        end
-    end)
-end)
-vim.keymap.set('n', '<leader>ggrc', function() vim.cmd("Git rebase --continue") end)
-vim.keymap.set('n', '<leader>ggpu', function() vim.cmd("Git push") end)
-vim.keymap.set('n', '<leader>ggs', vim.cmd.Git)
-
 -- Arc customizations
 vim.keymap.set('n', "<leader>ad", create_diff)
 vim.keymap.set('n', '<leader>ud', function()
--- dot_config/nvim/lua/plugins/fugitive.lua
+++ dot_config/nvim/lua/plugins/fugitive.lua
@@ -1,3 +1,22 @@
 return {
     'tpope/vim-fugitive',
+    config = function()
+        vim.keymap.set('n', '<leader>ggu', function()
+            vim.cmd("Git add --update")
+            vim.notify('git added all updated files')
+        end)
+        vim.keymap.set('n', '<leader>gga', function() vim.cmd("Git commit --amend --no-edit") end)
+        vim.keymap.set('n', '<leader>ggc', function()
+            vim.ui.input({ prompt = 'Commit message: ' }, function(input)
+                if input then
+                    vim.cmd('Git commit -m "' .. input .. '"')
+                else
+                    vim.notify("Commit aborted")
+                end
+            end)
+        end)
+        vim.keymap.set('n', '<leader>ggrc', function() vim.cmd("Git rebase --continue") end)
+        vim.keymap.set('n', '<leader>ggpu', function() vim.cmd("Git push") end)
+        vim.keymap.set('n', '<leader>ggs', vim.cmd.Git)
+    end
 }
