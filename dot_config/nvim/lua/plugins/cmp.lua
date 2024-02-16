c3b844f12832e21a6bd8f5131174b643621184a5
Kurtis Nusbaum <kcommiter@gmail.com>
2024-02-16 15:01:26 -0800

    cmp improvements
    - add "super tab" so snippets actually work
    - prioritize and order cmp sources

--- dot_config/nvim/lua/plugins/cmp.lua
+++ dot_config/nvim/lua/plugins/cmp.lua
@@ -1,3 +1,9 @@
+local has_words_before = function()
+    unpack = unpack or table.unpack
+    local line, col = unpack(vim.api.nvim_win_get_cursor(0))
+    return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
+end
+
 return {
     'hrsh7th/nvim-cmp',
     dependencies = {
@@ -11,6 +17,8 @@ return {
     },
     config = function()
         local cmp = require('cmp')
+        local lspkind = require('lspkind')
+        local luasnip = require('luasnip')
         cmp.setup({
             snippet = {
                 expand = function(args)
@@ -23,14 +31,37 @@ return {
                 ['<C-Space>'] = cmp.mapping.complete(),
                 ['<C-e>'] = cmp.mapping.abort(),
                 ['<CR>'] = cmp.mapping.confirm({ select = true }),
+                ["<Tab>"] = cmp.mapping(function(fallback)
+                    if cmp.visible() then
+                        cmp.select_next_item()
+                    elseif luasnip.expand_or_jumpable() then
+                        luasnip.expand_or_jump()
+                    elseif has_words_before() then
+                        cmp.complete()
+                    else
+                        fallback()
+                    end
+                end, { "i", "s" }),
+                ["<S-Tab>"] = cmp.mapping(function(fallback)
+                    if cmp.visible() then
+                        cmp.select_prev_item()
+                    elseif luasnip.jumpable(-1) then
+                        luasnip.jump(-1)
+                    else
+                        fallback()
+                    end
+                end, { "i", "s" }),
+
             }),
             sources = cmp.config.sources({
                 { name = 'nvim_lsp' },
                 { name = 'nvim_lsp_signature_help' },
-                { name = 'buffer' },
-                { name = 'emoji' },
-                { name = 'path' },
                 { name = 'luasnip' },
+            }, {
+                { name = 'buffer' },
+                { name = 'path' },
+            }, {
+                { name = 'emoji' },
             }),
             window = {
                 completion = cmp.config.window.bordered(),
