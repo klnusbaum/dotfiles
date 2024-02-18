5af5dfb60896e078d3164c35c3e4cee05b00869f
Kurtis Nusbaum <kcommiter@gmail.com>
2024-02-18 12:57:04 -0800

    Add fugitive keymaps
    Add fugitive keymaps for stashing and diffing.

--- dot_config/nvim/lua/plugins/fugitive.lua
+++ dot_config/nvim/lua/plugins/fugitive.lua
@@ -10,5 +10,11 @@ return {
         vim.keymap.set('n', '<leader>gp', function() vim.cmd("Git pull --rebase") end)
         vim.keymap.set('n', '<leader>gP', function() vim.cmd("Git push") end)
         vim.keymap.set('n', '<leader>gs', vim.cmd.Git)
+        vim.keymap.set('n', '<leader>gS', function()
+            vim.cmd("Git stash")
+        end)
+        vim.keymap.set('n', '<leader>gd', function()
+            vim.cmd("Git diff")
+        end)
     end
 }
