280692948102863915e5638119b537ce10ae92d5
Kurtis Nusbaum <kcommiter@gmail.com>
2024-02-17 17:25:52 -0800

    Update fugitive keymaps

--- dot_config/nvim/lua/plugins/fugitive.lua
+++ dot_config/nvim/lua/plugins/fugitive.lua
@@ -1,22 +1,14 @@
 return {
     'tpope/vim-fugitive',
     config = function()
-        vim.keymap.set('n', '<leader>ggu', function()
+        vim.keymap.set('n', '<leader>gu', function()
             vim.cmd("Git add --update")
             vim.notify('git added all updated files')
         end)
-        vim.keymap.set('n', '<leader>gga', function() vim.cmd("Git commit --amend --no-edit") end)
-        vim.keymap.set('n', '<leader>ggc', function()
-            vim.ui.input({ prompt = 'Commit message: ' }, function(input)
-                if input ~= nil and input ~= "" then
-                    vim.cmd('Git commit -m "' .. input .. '"')
-                else
-                    vim.notify("Commit aborted")
-                end
-            end)
-        end)
-        vim.keymap.set('n', '<leader>ggrc', function() vim.cmd("Git rebase --continue") end)
-        vim.keymap.set('n', '<leader>ggpu', function() vim.cmd("Git push") end)
-        vim.keymap.set('n', '<leader>ggs', vim.cmd.Git)
+        vim.keymap.set('n', '<leader>ga', function() vim.cmd("Git commit --amend --no-edit") end)
+        vim.keymap.set('n', '<leader>grc', function() vim.cmd("Git rebase --continue") end)
+        vim.keymap.set('n', '<leader>gp', function() vim.cmd("Git pull --rebase") end)
+        vim.keymap.set('n', '<leader>gP', function() vim.cmd("Git push") end)
+        vim.keymap.set('n', '<leader>gs', vim.cmd.Git)
     end
 }
