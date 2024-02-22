07db493f9dcce8cb3a28f20a6e7ecc8b83c0ba5f
Kurtis Nusbaum <kcommiter@gmail.com>
2024-02-22 10:15:45 -0800

    Treesitter TextObjects
    Try adding some treesitter text objects.

--- dot_config/nvim/lua/plugins/treesitter.lua
+++ dot_config/nvim/lua/plugins/treesitter.lua
@@ -1,5 +1,8 @@
 return {
     "nvim-treesitter/nvim-treesitter",
+    dependencies = {
+        'nvim-treesitter/nvim-treesitter-textobjects',
+    },
     build = ":TSUpdate",
     config = function()
         require("nvim-treesitter.configs").setup({
@@ -12,6 +15,44 @@ return {
             auto_install = true,
             highlight = {
                 enabled = true,
+            },
+            textobjects = {
+                select = {
+                    enable = true,
+                    lookahead = true,
+                    keymaps = {
+                        ["af"] = "@function.outer",
+                        ["if"] = "@function.inner",
+                        ["ab"] = "@block.outer",
+                        ["ib"] = "@block.inner",
+                    },
+                },
+                move = {
+                    enable = true,
+                    set_jumps = true,
+                    goto_next_start = {
+                        ["]m"] = "@function.outer",
+                        ["]]"] = "@class.outer",
+                    },
+                    goto_next_end = {
+                        ["]M"] = "@function.outer",
+                        ["]["] = "@class.outer",
+                    },
+                    goto_previous_start = {
+                        ["[m"] = "@function.outer",
+                        ["[["] = "@class.outer",
+                    },
+                    goto_previous_end = {
+                        ["[M"] = "@function.outer",
+                        ["[]"] = "@class.outer",
+                    },
+                    goto_next = {
+                        ["]d"] = "@conditional.outer",
+                    },
+                    goto_previous = {
+                        ["[d"] = "@conditional.outer",
+                    }
+                },
             },
         })
     end,
