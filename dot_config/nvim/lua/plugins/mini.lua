efaa7b33786117112f81677424c4d0cda5f6fabc
Kurtis Nusbaum <kcommiter@gmail.com>
2025-09-23 09:37:16 -0700

    switch to nvim-tree

--- dot_config/nvim/lua/plugins/mini.lua
+++ dot_config/nvim/lua/plugins/mini.lua
@@ -18,16 +18,5 @@ return {
                 end
             end)
         end, { desc = 'Create new session' })
-
-        -- Files
-        local mini_files = require('mini.files')
-        mini_files.setup()
-        vim.keymap.set('n', '<leader>fr', function()
-            if not mini_files.close() then mini_files.open() end
-        end)
-        vim.keymap.set('n', '<leader>fd', function()
-            local cur_file = vim.api.nvim_buf_get_name(0)
-            if not mini_files.close() then mini_files.open(cur_file) end
-        end)
     end
 }
---
+++ dot_config/nvim/lua/plugins/tree.lua
@@ -0,0 +1,27 @@
+return {
+    'nvim-tree/nvim-tree.lua',
+    config = function()
+        require('nvim-tree').setup()
+        local api = require('nvim-tree.api')
+
+        -- nvim-tree session restore workaround
+        vim.api.nvim_create_autocmd({ 'BufEnter' }, {
+            pattern = 'NvimTree*',
+            callback = function()
+                if not api.tree.is_visible() then
+                    api.tree.open()
+                end
+            end,
+        })
+
+        vim.keymap.set('n', '<leader>fd', function()
+            api.tree.find_file()
+        end)
+        vim.keymap.set('n', '<leader>ft', function()
+            api.tree.toggle()
+        end)
+        vim.keymap.set('n', '<leader>ff', function()
+            api.tree.focus()
+        end)
+    end
+}
