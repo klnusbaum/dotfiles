ce5bbc755728a97019fb1a383ff1ef8ecf3e787a
Kurtis Nusbaum <kcommiter@gmail.com>
2026-03-06 15:25:05 -0800

    cleanup lsp code comments and fixup lua-lsp config

--- dot_config/nvim/init.lua
+++ dot_config/nvim/init.lua
@@ -144,45 +144,42 @@ vim.api.nvim_create_autocmd('LspAttach', {
     end
 })
 
-local default_capabilities = require('cmp_nvim_lsp').default_capabilities()
-local lspconfig = require('lspconfig')
-lspconfig['gopls'].setup({ capabilities = default_capabilities })
-lspconfig['pylsp'].setup({ capabilities = default_capabilities })
-lspconfig['dockerls'].setup({ capabilities = default_capabilities })
-lspconfig['rust_analyzer'].setup({ capabilities = default_capabilities })
-lspconfig['ts_ls'].setup({ capabilities = default_capabilities })
-lspconfig['bashls'].setup({ capabilities = default_capabilities })
-lspconfig['cssls'].setup({ capabilities = default_capabilities })
-lspconfig['html'].setup({ capabilities = default_capabilities })
-lspconfig['jsonls'].setup({ capabilities = default_capabilities })
-lspconfig['tailwindcss'].setup({ capabilities = default_capabilities })
-lspconfig['templ'].setup({ capabilities = default_capabilities })
-lspconfig['terraformls'].setup({ capabilities = default_capabilities })
-lspconfig['yamlls'].setup({ capabilities = default_capabilities })
-lspconfig['lua_ls'].setup({
-    capabilities = default_capabilities,
+-- Configure lua_ls for Neovim development
+vim.lsp.config('lua_ls', {
     settings = {
         Lua = {
             runtime = {
-                -- Tell the language server which version of Lua you're using (most likely LuaJIT in the case of Neovim)
                 version = 'LuaJIT',
             },
             diagnostics = {
-                -- Get the language server to recognize the `vim` global
                 globals = { 'vim' },
             },
             workspace = {
-                -- Make the server aware of Neovim runtime files
                 library = vim.api.nvim_get_runtime_file('', true),
-                checkThirdParty = false, -- turn off to avoid third part check prompt
+                checkThirdParty = false,
             },
-            -- Do not send telemetry data containing a randomized but unique identifier
             telemetry = {
                 enable = false,
             },
         },
     },
 })
+
+vim.lsp.enable('lua_ls')
+vim.lsp.enable('gopls')
+vim.lsp.enable('pylsp')
+vim.lsp.enable('dockerls')
+vim.lsp.enable('rust_analyzer')
+vim.lsp.enable('ts_ls')
+vim.lsp.enable('bashls')
+vim.lsp.enable('cssls')
+vim.lsp.enable('html')
+vim.lsp.enable('jsonls')
+vim.lsp.enable('tailwindcss')
+vim.lsp.enable('templ')
+vim.lsp.enable('terraformls')
+vim.lsp.enable('yamlls')
+
 
 -- Arc customizations
 vim.keymap.set('n', '<leader>ad', create_diff)
